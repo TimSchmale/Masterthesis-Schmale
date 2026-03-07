7a63611a1528109dff924c40e13639ab3305efaf
TimSchmale <tim.schmale@tu-dortmund.de>
2026-03-07 20:44:41 +0100

    Set up scoring function file and move column and row reduction methods into correct modeling approach file

--- column_row_reduction_study.py
+++ column_row_reduction_study.py
@@ -1,11 +1,137 @@
 import pandas as pd
 import random
-from functions import get_leverage_scores, get_cross_leverage_scores, get_random_scores, get_combined_scores, \
-    row_reduction, column_reduction
+from scoring_functions import get_leverage_scores, get_cross_leverage_scores, get_random_scores, get_combined_scores
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error
 import time
 from visualizations import *
+
+# ------------------------------------------------------------
+# Function to perform a column reduction given score values for each column as calculation basis
+#
+# INPUT:
+#   X         : data matrix of dimension n x p
+#   scores    : scoring vector of length p
+#   k         : rank parameter (desired rank of approximation)
+#
+# OUTPUT:
+#   reduced data matrix as well as vector of selected variable indices
+# ------------------------------------------------------------
+def column_reduction(X, scores, k):
+    # get dimensions
+    n, p = np.shape(X)
+    # get probabilities
+    probs = scores / np.sum(scores)
+
+    # get the number of desired columns
+    c = int(np.ceil(k * np.log(k)))
+
+    # scale probs and set maximum to 1
+    scaled_probs = np.minimum(c * probs, 1)
+
+    # sample the columns and fill S and D
+    t = 0
+    sampled_cols = []
+    S_cols = []
+    D_diag = []
+
+    for j in range(p):
+        z = np.random.uniform(0, 1)
+        if z <= scaled_probs[j]:
+            sampled_cols.append(j)
+            S_cols.append(j)
+            D_diag.append(1 / np.sqrt(scaled_probs[j]))
+            t += 1
+
+    # create S and D
+    t = len(sampled_cols)
+    S = np.zeros((p, t))
+    D = np.zeros((t, t))
+
+    for idx, j in enumerate(sampled_cols):
+        S[j, idx] = 1
+        D[idx, idx] = D_diag[idx]
+
+    # get C
+    X = np.array(X)
+    C = X @ S @ D
+
+    return {
+        "C": C,
+        "selected_columns": sampled_cols,
+        "probs": scaled_probs
+    }
+
+# ------------------------------------------------------------
+# Function to perform a row reduction based on a score vector
+#
+# INPUT:
+#   C         : reduced data matrix of dimension n x c
+#   y         : response vector of length n
+#   k         : rank parameter (desired rank of approximation)
+#
+# OUTPUT:
+#   reduced data matrix as well as vector of selected variable indices
+# ------------------------------------------------------------
+def row_reduction(C, y, k):
+    # get dimensions
+    n, c = np.shape(C)
+    # get the leverage scores for the rows
+    scores = get_leverage_scores(C, k)
+    # get probabilities
+    probs = scores / np.sum(scores)
+
+    # get the number of desired columns
+    r = int(np.ceil(c * np.log(c)))
+
+    # scale probs and set maximum to 1
+    scaled_probs = np.minimum(r * probs, 1)
+
+    # build mechanism when r > row number
+    if n < r:
+        #print("No row reduction needed. Original Matrix C kept.")
+        return {
+            "R": C,
+            "y": y,
+            "selected_rows": list(range(n)),
+            "probs": scaled_probs
+        }
+
+    # sample the rows and fill S and D
+    t = 0
+    sampled_rows = []
+    S_rows = []
+    D_diag = []
+
+    for j in range(n):
+        z = np.random.uniform(0, 1)
+        if z <= scaled_probs[j]:
+            sampled_rows.append(j)
+            S_rows.append(j)
+            D_diag.append(1 / np.sqrt(scaled_probs[j]))
+            t += 1
+
+    # create S and D
+    t = len(sampled_rows)
+    S = np.zeros((t, n))
+    D = np.zeros((t, t))
+
+    for idx, j in enumerate(sampled_rows):
+        S[idx, j] = 1
+        D[idx, idx] = D_diag[idx]
+
+    # get C
+    C = np.array(C)
+    R = D @ S @ C
+
+    # get the reduced y
+    y_reduced = y.iloc[sampled_rows]
+    return {
+        "R": R,
+        "y": y_reduced,
+        "selected_rows": sampled_rows,
+        "probs": scaled_probs
+    }
 
 # ==============================================================================================
 # data_reduction: Function for data reduction of Simulation Study
